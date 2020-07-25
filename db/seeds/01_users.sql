6fe43511ca3762d17acfaf274bf15c979a7ef2c8
Rob <robpolisena@gmail.com>
2020-07-25 22:19:42 +0000

    added schema for table entities

--- db/seeds/01_users.sql
+++
@@ -1,5 +0,0 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
-INSERT INTO users (name) VALUES ('Rob');
-INSERT INTO users (name) VALUES ('Kate');
