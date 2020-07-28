5534968a423948eacc9a80d566c4216dd2920f57
Rob <robpolisena@gmail.com>
2020-07-28 19:34:17 +0000

    schema updated, seed data updated to remove primary key, updated root /quizzes which contains the create a quiz functions

--- db/seeds/02_categories.sql
+++ db/seeds/02_categories.sql
@@ -1,5 +1,5 @@
-insert into categories (id, name) values (1, 'Sports');
-insert into categories (id, name) values (2, 'Animals');
-insert into categories (id, name) values (3, 'Vehicles');
-insert into categories (id, name) values (4, 'Celebrities');
-insert into categories (id, name) values (5, 'Entertainment: Films');
+insert into categories (name) values ('Sports');
+insert into categories (name) values ('Animals');
+insert into categories (name) values ('Vehicles');
+insert into categories (name) values ('Celebrities');
+insert into categories (name) values ('Entertainment: Films');
