d4a9a632e92dc0e3d92345e497bdecfd5821937a
Oluwatobii <bellotobi10@gmail.com>
2020-07-29 03:01:48 +0000

    Using Promise to insert queries into the database

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -17,11 +17,6 @@ module.exports = (db) => {
   // the route should be /quizzes
 
   router.post("/", (req, res) => {
-    //  return pool.query(queryRequest, [user.name, user.email, user.password])
-    //   .then(res => res.rows[0]);
-    //res.render("create_quiz_form");
-    //console.log(req.body)
-
     const {
       quiz_name,
       quiz_category,
@@ -89,80 +84,95 @@ module.exports = (db) => {
       question5_answer,
     ];
 
+    //Function to insert the Quiz
     const insertQuiz = function (quizValues) {
       const quizQuerry = `INSERT INTO quizzes (owner_id, name, category_id, isPublic, isReady, date_created, points_allocated) VALUES ($1, $2, $3, $4, $5, now()::date, $6) RETURNING id AS quiz_id;`;
       //return pool
       return db
         .query(quizQuerry, quizValues)
         .then((quizRes) => {
-          console.log("Quiz ID Created", quizRes.rows, quizRes.rows[0].quiz_id);
+          //console.log("Quiz ID Created", quizRes.rows, quizRes.rows[0].quiz_id);
           return quizRes.rows[0];
         })
         .catch((err) => console.error("query error", err.stack));
     };
 
+    //Function to insert the questions
     const insertQuestions = function (questionValues) {
-      console.log(`question values: ${questionValues}`);
+      //console.log(`question values: ${questionValues}`);
       const questionQuerry = `INSERT INTO questions (question) VALUES ($1), ($2), ($3), ($4), ($5) RETURNING id as question_id;`;
       return db
         .query(questionQuerry, questionValues)
         .then((questionRes) => {
-          // console.log(`question1 id: ${JSON.stringify(questionRes.rows[0].question_id)}`)
-          // console.log(`question2 id: ${JSON.stringify(questionRes.rows[1].question_id)}`)
-          // console.log(`question3 id: ${JSON.stringify(questionRes.rows[2].question_id)}`)
-          // console.log(`question4 id: ${JSON.stringify(questionRes.rows[3].question_id)}`)
-          // console.log(`question5 id: ${JSON.stringify(questionRes.rows[4].question_id)}`)
-          return res.rows;
+          return questionRes.rows;
         })
         .catch((err) => console.error("query error", err.stack));
     };
 
-    // INSERT INTO quiz_question (quiz_id, question_id) VALUES ();
-    // INSERT INTO question_answer (question_id, answer_id) VALUES ();
-    // INSERT INTO question_option (question_id, option_id) VALUES ();
-    // INSERT INTO question_option (question_id, option_id) VALUES ();
-    // INSERT INTO question_option (question_id, option_id) VALUES ();
-    // const quizQuestionValue = [
-    //   quizRes.rows[0].quiz_id,
-    //   questionRes.rows[0].question_id,
-    // ];
+    // Function to insert the options
+    const insertOptions = function (optionValues) {
+      const query = `INSERT INTO options (option) VALUES ($1), ($2), ($3), ($4), ($5), ($6), ($7), ($8), ($9), ($10), ($11), ($12), ($13), ($14), ($15), ($16), ($17), ($18), ($19), ($20) RETURNING id AS option_id`;
+      return db
+        .query(query, optionValues)
+        .then((result) => {
+          return result.rows;
+        })
+        .catch((err) => console.error("query error", err.stack));
+    };
 
-    const insertQuizQuestion = function (quizQuestionValue) {
+    //Function to insert the quiz_question table
+    const insertQuizQuestion = function (quizId, questionId) {
       const quizQuestionQuerry = `INSERT INTO quiz_question (quiz_id, question_id) VALUES ($1, $2) RETURNING id AS quizQuestion_id;`;
       return db
-        .query(quizQuestionQuerry, quizQuestionValue)
+        .query(quizQuestionQuerry, [quizId, questionId])
         .then((quizQuestionRes) => {
           console.log("quizQuestionRes", quizQuestionRes.rows[0]);
-          return quizQuestionRes.rows;
+          return quizQuestionRes.rows[0];
         })
         .catch((err) => console.error("query error", err.stack));
     };
 
+    //Function to insert all the quiz questions.
+    const insertAllQuizQuestion = function (quiz_id, questionsIds) {
+      let result = [];
+      for (let questionIdObj of questionsIds) {
+        result.push(insertQuizQuestion(quiz_id, questionIdObj.question_id));
+      }
+      return result;
+    };
 
     //This is where the promises starts
-    insertQuiz(quizValues)
-      .then(({ quiz_id }) => {
-        console.log(`QuizId ${quiz_id}`);
-        insertQuestions(questionValues);
-        return quiz_id;
+    Promise.all([
+      insertQuiz(quizValues),
+      insertQuestions(questionValues),
+      insertOptions(optionValues),
+    ])
+      .then(([{ quiz_id }, questionIds, optionsIds]) => {
+        // processing here
+        console.log(
+          "THE QUIZES ID: ",
+          quiz_id,
+          "THE QUESTIONS ID: ",
+          questionIds,
+          "THE OPTIONS ID: ",
+          optionsIds
+        );
+        return insertAllQuizQuestion(quiz_id, questionIds);
       })
-      .then((quiz_id) => {
-        console.log(`Insert Questions Response: ${quiz_id}`);
-
-        insertQuizQuestion(quizQuestionValue).then((quizQuestionResponse) => {
-          console.log("quizQuestionResponse", quizQuestionResponse);
-
-          return {
-            quiz_id,
-            quizQuestionResponse,
-          };
-        });
-      })
-      .catch((e) => {
-        console.log(`err catch:${e}`);
-        res.send(e);
-      });
+      .catch((err) => console.error("query error", err.stack));
   });
+
+  // INSERT INTO quizzes (owner_id, name, category_id, isPublic, isReady, date_created, points_allocated) VALUES ($1, $2, $3, $4, $5, now()::date, $6) RETURNING id AS quiz_id; INSERT INTO quiz_question (quiz_id, question_id) VALUES ($1, $2) RETURNING id AS quizQuestion_id;INSERT INTO quiz_question (quiz_id, question_id) VALUES ();
+  // INSERT INTO question_answer (question_id, answer_id) VALUES ();
+  // INSERT INTO question_option (question_id, option_id) VALUES ();
+  // INSERT INTO question_option (question_id, option_id) VALUES ();
+  // INSERT INTO question_option (question_id, option_id) VALUES ();
+  // const quizQuestionValue = [
+  //   quizRes.rows[0].quiz_id,
+  //   questionRes.rows[0].question_id,
+  // ];
+
+  // INSERT INTO quizzes (...) VALUES (...); INSERT INTO quiz_questions () VALUES ();
 
   return router;
 };
